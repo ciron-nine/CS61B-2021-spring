f08974a2d41ce67ceb6db4373af30fe3bc6e21d6
cirno-nine <ghost2583855829@outlook.com>
2024-03-11 19:55:59 +0800

    lab2 done by cirno

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,11 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimesDouble() {
+        IntList lst = IntList.of(14, 15, 16, 17,17, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 289 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
 }
