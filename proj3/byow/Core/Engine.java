c37a9f12633cfdfc329df6d9da834fb0e5072b4a
cirno-nine <ghost2583855829@outlook.com>
2024-04-05 15:24:12 +0800

    project 3 test

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -2,6 +2,10 @@ package byow.Core;
 
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+import org.antlr.v4.runtime.misc.Pair;
+
+import java.util.Random;
 
 public class Engine {
     TERenderer ter = new TERenderer();
@@ -13,6 +17,66 @@ public class Engine {
      * Method used for exploring a fresh world. This method should handle all inputs,
      * including inputs from the main menu.
      */
+    private static int route(int x, int y, int width, TETile[][] tiles) {
+
+
+        return 0;
+    }
+
+    private static Pair<Integer, Integer> init_room(int x, int y, int length, int width, TETile[][] tiles) {
+        for(int i = x; i < x + width; i ++) {
+            tiles[i][y] = Tileset.WALL;
+        }
+        for(int i = y + 1; i < y + length; i ++) {
+            tiles[x][i] = Tileset.WALL;
+        }
+        for(int i = x; i < x + width; i ++) {
+            tiles[i][y + length - 1] = Tileset.WALL;
+        }
+        for(int i = y + 1; i < y + length; i ++) {
+            tiles[x + width - 1][i] = Tileset.WALL;
+        }
+        for(int i = x + 1; i < x + width - 1; i ++) {
+            for(int j = y + 1; j < y + length - 1;j ++) {
+                tiles[i][j] = Tileset.FLOOR;
+            }
+        }
+        tiles[x + 1][y] = Tileset.LOCKED_DOOR;
+        int FLOOR_x = x + width / 2;
+        int FLOOR_y = y + length - 1;
+        tiles[FLOOR_x][FLOOR_y] = Tileset.FLOOR;
+        Pair<Integer, Integer> pair = new Pair<>(FLOOR_x, FLOOR_y);
+        return pair;
+    }
+    private static void make_new_route(int x, int y, TETile[][] tiles) {
+
+    }
+
+    private static void new_world(Random random, TETile[][] tiles) {
+        int init_x = random.nextInt(60);
+        int init_y = random.nextInt(20);
+        int init_width = 3 + random.nextInt( 7);
+        int init_height = 3 + random.nextInt(2);
+        Pair<Integer, Integer> init_room = init_room(init_x, init_y, init_height, init_width, tiles);
+
+        int room_count = random.nextInt(7);
+
+        int pool_count = random.nextInt(5);
+        int sum_count = room_count + pool_count;
+        while (sum_count != 0) {
+            int select = random.nextInt(1);
+            if(select == 1) {
+                sum_count --;
+                pool_count --;
+            }
+            else {
+                room_count --;
+                sum_count --;
+            }
+        }
+
+    }
+
     public void interactWithKeyboard() {
     }
 
@@ -37,6 +101,7 @@ public class Engine {
      * @param input the input string to feed to your program
      * @return the 2D TETile[][] representing the state of the world
      */
+
     public TETile[][] interactWithInputString(String input) {
         // TODO: Fill out this method so that it run the engine using the input
         // passed in as an argument, and return a 2D tile representation of the
@@ -45,8 +110,28 @@ public class Engine {
         //
         // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
         // that works for many different input types.
-
-        TETile[][] finalWorldFrame = null;
+        ter.initialize(WIDTH, HEIGHT);
+        boolean is_new = false;
+        long seed = 0;
+        for(int i = 0; i < input.length(); i ++) {
+            if(input.charAt(i) == 'N' || input.charAt(i) == 'n') {
+                is_new = true;
+                i ++;
+                while(input.charAt(i) != 'S' && input.charAt(i) != 's') {
+                    seed = seed * 10 + input.charAt(i) - '0';
+                    i ++;
+                }
+            }
+        }
+        TETile[][] finalWorldFrame = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                finalWorldFrame[x][y] = Tileset.NOTHING;
+            }
+        }
+        Random SEED = new Random(seed);
+        new_world(SEED, finalWorldFrame);
+        ter.renderFrame(finalWorldFrame);
         return finalWorldFrame;
     }
 }
